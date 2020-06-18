eb78eaebdd489af6facfd3b8dc293f8863d4c27a
emilio-matics <53688141+emilio-matics@users.noreply.github.com>
2020-06-18 15:06:54 +0300

    router works

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import { UIRouterModule } from "@uirouter/angular";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+
+const usersState = { name: "users", url: "/users", component: UsersComponent };
+
+
 
 @NgModule({
    declarations: [
@@ -14,7 +19,8 @@ import { UsersComponent } from './users/users.component';
    imports: [
       BrowserModule,
       AppRoutingModule,
-      HttpClientModule
+      HttpClientModule,
+      UIRouterModule.forRoot({ states: [usersState], useHash: true })
    ],
    providers: [],
    bootstrap: [
