0af72e118e077da635c8cafdc13d67bf26e80ab8
emilio-matics <53688141+emilio-matics@users.noreply.github.com>
2020-06-18 17:34:42 +0300

    finished

--- app/src/app/pictures/pictures.component.ts
+++ app/src/app/pictures/pictures.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-pictures',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PicturesComponent implements OnInit {
 
-  constructor() { }
+  pics: any;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getPics();
+  }
+
+  getPics() {
+    this.http.get('http://localhost:5000/project/pics').subscribe(response => {
+      this.pics = response;
+      console.log(`response ${JSON.stringify(response[0])}`);
+    },error=>{console.log(`error:${error}`)});
   }
 
 }
